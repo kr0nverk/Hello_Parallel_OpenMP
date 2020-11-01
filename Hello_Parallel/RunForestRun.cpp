0e31fb0ef41b58d688555e3fbde8fbe3864cf96c
kr0nverk <rusakov.aleksej94@gmail.com>
2020-11-01 12:39:55 +0300

    no message

--- Hello_Parallel/RunForestRun.cpp
+++ Hello_Parallel/RunForestRun.cpp
@@ -8,27 +8,30 @@ using namespace std;
 
 
 int main() {
-
-	ofstream fout;
-    fout.open("C:\\Users\\vovan\\source\\repos\\Hello_Parallel\\RunForestRun\\a.txt");
-
-    if (fout.is_open())
-    {
+/*
+    ofstream fout("C:\\Users\\vovan\\source\\repos\\Hello_Parallel\\RunForestRun\\a.txt");
+    if (fout.is_open()) {
         double temp = 123456789.;
         fout << fixed << setprecision(0) << temp;
     }
+    fout.close();
+    */
 
-    #pragma omp parallel section
+    #pragma omp parallel sections
     {
         #pragma omp section
         {
-
-        }
-        #pragma omp section
-        {
-
+            char temp;
+            int count = 0;
+            ifstream inf("C:\\Users\\vovan\\source\\repos\\Hello_Parallel\\RunForestRun\\a.txt");
+            if (inf.is_open()) {
+                while (inf.get(temp) && count < 2) {
+                    cout << temp;
+                    count++;
+                }
+                inf.close();
+            }
         }
     }
-
 	return 0;
 }
